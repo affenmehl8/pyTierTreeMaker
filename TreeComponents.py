b5fafc841ffad9ca12702cde39037cd9742bcc2a
affenmehl8 <andreas.schwarz99@outlook.com>
2022-11-07 22:00:20 +0100

    +placement of nodes in tiers correctly positioned

--- TreeComponents.py
+++ TreeComponents.py
@@ -1,21 +1,20 @@
 from tkinter import *
 
 class Node:
-    height_const = 1
+    __height_const = 1
+#todo minimize width with text
+#todo set maxwidth
+    def __init__(self, master, width, text = ""):
+        self.frame = Frame(master, highlightbackground="black", highlightthickness=1)
 
-    def __init__(self, text = "" ):
-        self.frame = Frame(highlightbackground="black", highlightthickness=1)
-
-
-
-        empty_text = Text(self.frame, height=self.height_const, state="disabled")
+        empty_text = Text(self.frame, height=self.__height_const, state="disabled", width=width)
         empty_text.grid(column=0, row=0)
 
         check = Checkbutton(self.frame)
         check.grid(column=1, row=0)
 
-        main_text = Text(self.frame, height=3 * self.height_const)
-        main_text.insert(END, "bla bla bla bla")
+        main_text = Text(self.frame, height=3 * self.__height_const, width=width)
+        main_text.insert(END, text)
         main_text.grid(column=0, row=1)
 
     def get_node(self):
--- mainWindow.py
+++ mainWindow.py
@@ -1,25 +1,18 @@
-import logging
-import time
 import warnings
-from tkinter import *
-from tkinter import ttk
-
 from TreeComponents import *
 
 class MainWindow:
     latin_numbers = ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX')
+    __tier_font_size = 30
+    __tiers_list = []
 
-    def __init__(self, y=0, x=0):
+    def __init__(self, height=360, width=480, one_tier_width = 500):
 
-        self.height = y
-        self.width = x
-        resolution=""
-        if self.height==0 or self.width==0:
-            self.height = 360
-            self.width = 480
-        resolution="{}x{}".format(self.width,self.height)
+        self.__one_tier_height = height
+        self.__one_tier_width = one_tier_width
+
         self.root = Tk()
-        self.root.geometry(resolution)
+        self.root.geometry("{0}x{1}".format(width, height))
 
         self.__create_menu()
         self.__create_tiers()
@@ -43,17 +36,14 @@ class MainWindow:
 
 
     def __create_tiers(self, tiercount = 10):
-
         if tiercount > 20:
             warnings.warn("Max value for tiercount is 20!")
             tiercount = 10
         canvas = Canvas(self.root)
 
-
         def _configure(event):
             # print(canvas.winfo_width(), canvas.winfo_height())
-            canvas.configure(scrollregion=(0,0,9999,9999)) #x,y,x,y
-
+            canvas.configure(scrollregion=(0,0,self.__one_tier_width*tiercount,self.__one_tier_height)) #x1,y1,x2,y2
         canvas.bind('<Configure>', _configure)
 
         xscrollbar = Scrollbar(self.root, orient="horizontal", command=canvas.xview)
@@ -63,23 +53,25 @@ class MainWindow:
         canvas.pack(fill="both", expand=True)
         canvas.configure(xscrollcommand=xscrollbar.set, yscrollcommand=yscrollbar.set)
 
-
-        for x in range(tiercount): #todo try to remove hardcoded sizes, fixed sized for frames and it'S contents!
+        for x in range(tiercount):
             frame = Frame(canvas, highlightbackground="black", highlightthickness=1)
-            title_text = Text(frame, height=1, font=('Times New Roman', 30, 'bold'), highlightthickness=1)
+
+            title_text = Text(frame, height=0, font=('Times New Roman', self.__tier_font_size, 'bold'), highlightthickness=1)
             title_text.insert(END, self.latin_numbers[x])
             title_text.configure(state="disabled")
             title_text.pack(side=TOP)
 
-            field_text = Text(frame)
-            field_text.insert(END, "blalbablalbalbalbalb")
-            field_text.pack(side=TOP)
+            tier_canvas = Canvas(frame, width=self.__one_tier_width, height=self.__one_tier_height)
+            tier_canvas.pack(side=TOP, anchor="nw")
 
-            # frame.pack(side=LEFT)
-            canvas.create_window(x*500, 0, window=frame, anchor='nw')
+            canvas.create_window(x*self.__one_tier_width, 0, window=frame, anchor='nw')
+            self.__tiers_list.append(tier_canvas)
 
+    def add_node_to_tier(self, tier_num, node_text):
+        canvas = self.__tiers_list[tier_num-1]
 
-
+        node = Node(canvas, int(self.__one_tier_width/9), node_text) #don't ask
+        node.get_node().pack(side=TOP, anchor="nw", padx=10, pady=10)
 
     def run(self):
         self.root.mainloop()
