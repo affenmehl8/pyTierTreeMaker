dd650ddc334bcda92743a941f9df8fab62aab7f3
affenmehl8 <andreas.schwarz99@outlook.com>
2022-12-06 19:56:21 +0100

    +scrollbars finally working for now at least .....

--- dialogWindows.py
+++ dialogWindows.py
@@ -1,3 +1,6 @@
+from tkinter import Tk, Canvas, LAST, simpledialog, Text, END
+from tkinter.ttk import Entry
+
 #todo onclose save unchanged
 #todo die interaktionen für menubars
 # from tkinter import simpledialog
@@ -10,3 +13,30 @@
 #     def new_file(self):
 #         answer = simpledialog.askstring("New Tree", "How many tiers do you want? (max. 20)")
 #         self.master.__create_tiers()
+
+
+class AddRemoveNode:
+    def __init__(self):
+        self.root = Tk()
+        self.canvas = Canvas(self.root)
+        self.canvas.configure(highlightthickness=5, highlightbackground="green")
+        self.canvas.pack(expand=True, fill="both")
+        self.tmpfct()
+
+    def tmpfct(self):
+        height = 5
+        width = 5
+
+        b = Text(self.canvas, height=5, width=10)
+        b.insert(END, "Tiers\n\n\nNodes")
+        b.grid(row=0, column=0)
+        b.configure(state="disabled")
+        for i in range(height):  # Rows
+            for j in range(1, width):  # Columns
+                b = Text(self.canvas, height=5, width=10)
+                b.insert(END, "sssesesss")
+                b.grid(row=i, column=j)
+                b.configure(state="disabled")
+
+    def run(self):
+        self.root.mainloop()
--- mainWindow.py
+++ mainWindow.py
@@ -1,15 +1,21 @@
+import tkinter
 import warnings
 from tkinter import simpledialog, messagebox
 
 import dialogWindows
 from TreeComponents import *
+from dialogWindows import AddRemoveNode
 
+
+class Arrow:
+    pass#todo contains arrow itself (if possible), left node, right node
 
 class MainWindow:
     latin_numbers = ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX')
     __max_tiers = 20
     __tier_font_size = 30
     __tiers_list = [] #ICH BRAUCH HIER NEN 2D Array anderst gehts nich, um die höhen der einzelnen nodes zu bestimmen und zu addieren, damit die schwule leiste richtig funktioniert!!!!!!!!!!!!!!!!!!!!!
+    __tiercount = 10
 
     def __init__(self, height=360, width=480, one_tier_width = 500):
 
@@ -19,8 +25,14 @@ class MainWindow:
         self.root = Tk()
         self.root.geometry("{0}x{1}".format(width, height))
 
+        self.main_canvas = Canvas(self.root)
+        self.__xscrollbar = Scrollbar(self.root, orient="horizontal", command=self.main_canvas.xview)
+        self.__xscrollbar.pack(side=BOTTOM, fill=X)
+        self.__yscrollbar = Scrollbar(self.root,orient="vertical", command=self.main_canvas.yview)
+        self.__yscrollbar.pack(side=RIGHT, fill=Y)
+        self.main_canvas.configure(xscrollcommand=self.__xscrollbar.set, yscrollcommand=self.__yscrollbar.set)
+
         self.__create_menu()
-        #self.__create_tiers()
 
     def new_file(self):
         answer = None
@@ -36,7 +48,31 @@ class MainWindow:
                 print(answer, type(answer))
             except ValueError:
                     messagebox.showwarning(title="Warning", message=error_message)
-        self.__create_tiers(answer)
+#todo if ok : das unten machen, if cancel, break!
+
+        #clear everything before creating new
+        self.main_canvas.delete("all")
+        self.__create_tiers(answer) #TODO falls schon was auf dem canvas existiert ne box mit z.b. all unsaved changes of the current tree will be lost (Ok, Cancel)
+
+    def open_file(self):
+        pass
+    def save_file(self):
+        pass
+
+    def add_node(self):
+        pass
+    def remove_node(self):
+        pass
+    def add_arrow(self):
+        add_remove_node = AddRemoveNode()
+        add_remove_node.run()
+    def remove_arrow(self):
+        pass
+    def add_tier(self):
+        pass
+    def remove_tier(self):
+        pass#todo tricky weil dependences, dann alle nodes da auch löschen und alle abh. pfeile
+
 
     def __create_menu(self):
         menubar = Menu()
@@ -50,13 +86,10 @@ class MainWindow:
         menubar.add_cascade(label="File", menu=menu_file)
 
         menu_edit = Menu(menubar)
-        menu_edit.add_command(label="Add Node")
-        menu_edit.add_command(label="Add Arrow")
-        menu_edit.add_command(label="Add Tier")
-        menu_edit.add_command(label="Insert Tier")
+        menu_edit.add_command(label="Add/Remove Node", command=lambda : self.add_remove_arrow())
+        menu_edit.add_command(label="Add/Remove Arrow")
         menu_edit.add_separator()
-        menu_edit.add_command(label="Remove Node")
-        menu_edit.add_command(label="Remove Arrow")
+        menu_edit.add_command(label="Add Tier")
         menu_edit.add_command(label="Remove Tier") #TODO if contains nodes, are you sure blabla dialogwindow
         menubar.add_cascade(label="Edit", menu=menu_edit)
 
@@ -64,29 +97,22 @@ class MainWindow:
     def __create_tiers(self, tiercount = 10):
         if tiercount > self.__max_tiers:
             warnings.warn("Max value for tiercount is {}!".format(self.__max_tiers))
-            tiercount = 10
-        canvas = Canvas(self.root)
-
+        else:
+            __tiercount = tiercount
         def _configure(event):
+            print("EVENT FIRED!!!!!")
             max_amt_nodes = self.get_max_height()
             node_height = Node.get_height(self)
-            canvas.configure(scrollregion=(0,0,self.__one_tier_width*tiercount,canvas.winfo_height()+ max_amt_nodes*node_height)) #x1,y1,x2,y2 #todo maybe change __height to winfo_height()
+            self.main_canvas.configure(scrollregion=(0,0,self.__one_tier_width*__tiercount,self.main_canvas.winfo_height()+ max_amt_nodes*node_height)) #x1,y1,x2,y2 #todo maybe change __height to winfo_height()
         def _on_mousewheel(event):
-            canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
-        canvas.bind('<Configure>', _configure)
-        canvas.bind_all("<MouseWheel>", _on_mousewheel)
+            self.main_canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+        self.main_canvas.bind('<Configure>', _configure)
+        self.main_canvas.bind_all("<MouseWheel>", _on_mousewheel)
 
 
 
-        xscrollbar = Scrollbar(self.root, orient="horizontal", command=canvas.xview)
-        xscrollbar.pack(side=BOTTOM, fill=X)
-        yscrollbar = Scrollbar(self.root,orient="vertical", command=canvas.yview)
-        yscrollbar.pack(side=RIGHT, fill=Y)
-        canvas.pack(fill="both", expand=True)
-        canvas.configure(xscrollcommand=xscrollbar.set, yscrollcommand=yscrollbar.set)
-
-        for x in range(tiercount):
-            frame = Frame(canvas, highlightbackground="black", highlightthickness=0)
+        for x in range(__tiercount):
+            frame = Frame(self.main_canvas, highlightbackground="black", highlightthickness=0)
 
             #Top enumaration Numbers
             title_text = Text(frame, height=0, font=('Times New Roman', self.__tier_font_size, 'bold'), highlightthickness=1)
@@ -95,12 +121,14 @@ class MainWindow:
             title_text.pack(side=TOP)
 
             #Canvases for nodes
-            tier_canvas = Canvas(frame, width=self.__one_tier_width, height=self.__height)
+            tier_canvas = Canvas(frame, width=self.__one_tier_width, height=self.__height, bg="blue")
             tier_canvas.pack(side=TOP, anchor="nw")
 
-            canvas.create_window(x*self.__one_tier_width, 0, window=frame, anchor='nw')
+            self.main_canvas.create_window(x*self.__one_tier_width, 0, window=frame, anchor='nw')
 
             self.__tiers_list.append(tier_canvas)
+        self.main_canvas.pack(fill="both", expand=True)
+        self.main_canvas.event_generate("<Configure>")
 
     def add_node_to_tier(self, tier_num, node_text):
         sub_canvas = self.__tiers_list[tier_num-1]
